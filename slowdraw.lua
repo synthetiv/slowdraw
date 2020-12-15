c49d289e836ba796dc84894d8fad2d55013b43bc
Kenji Garland <kenjigarland@gmail.com>
2020-12-16 03:20:48 +0700

    Testing drawing CPU usage

---
+++ slowdraw.lua
@@ -0,0 +1,27 @@
+function init()
+	frame_metro = metro.init{
+		time = 1 / 16,
+		event = function()
+			redraw()
+		end
+	}
+	frame_metro:start()
+end
+
+function redraw()
+	screen.clear(j)
+	for x = 1, 45 do
+		for y = 1, 24 do
+			screen.rect(x * 3, y * 3 - 1, 1, 1)
+			screen.level(2)
+			screen.fill()
+		end
+	end
+	screen.update()
+end
+
+function cleanup()
+	if frame_metro ~= nil then
+		frame_metro:stop()
+	end
+end
